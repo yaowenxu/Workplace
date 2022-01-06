d3b7f142bc6f81dff0f4b897c1a8e5a77f93efc1
yaowenxu <yaowen1996@hotmail.com>
2022-01-06 18:05:42 +0800

    update
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

--- pytorch/rpc/worker.py
+++ pytorch/rpc/worker.py
@@ -6,8 +6,8 @@ os.environ['MASTER_ADDR'] = '127.0.0.1'
 os.environ['MASTER_PORT'] = '7030'
 
 # FIXME: 函数的定义必须在master和worker中都有，类似存根的概念;
+# TODO: 可以不同，但是函数名字要相同；
 def hello():
-    print("hello hushuang!")
     return
 
 # FIXME: rank数量还没达到 world size，程序会block;
