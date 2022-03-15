2257430cc87270279d828b848e9e4ae545ecd22f
yaowenxu <yaowen1996@hotmail.com>
2022-03-15 10:48:41 +0800

    update rust
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

--- understand-rust/hello_cargo/src/main.rs
+++ understand-rust/hello_cargo/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, yaowen!");
+    let name = "yaowen";
+    let name = name;
+    let guess: isize =  "666".parse().expect("not a number");
+    println!("Hello, {} {}!", name, guess);
+    function();
+}
+
+// 新的函数
+fn function() -> i32{
+    println!("Hello, func!");
+    let state = {
+        let mut x = 1;
+        println!("step 2 {}", x);
+        x = x + 1;
+        x
+    };
+    println!("step 1 {}", state);
+    return 0
 }
