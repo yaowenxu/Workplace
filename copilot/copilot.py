7c92d02c07619fe3e456cf408f6313d1314827e2
yaowenxu <yaowen_contact@163.com>
2022-12-23 13:57:30 +0800

    update copilot demo
    Signed-off-by: yaowenxu <yaowen_contact@163.com>

--- copilot/copilot.py
+++ copilot/copilot.py
@@ -6,7 +6,9 @@ import os
 # main function, prints the current directory and print hello world;
 def main():
     print(os.getcwd())
-    print("Hello World")
+    print(
+        "Hello World! This is a test message from the copilot package. If you see this message, the package is working correctly."
+    )
 
 # call main function
 if __name__ == "__main__":
